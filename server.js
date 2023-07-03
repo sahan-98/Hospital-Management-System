0cc23b0821c8c321dc1b23ed3893cd52d6daf5aa
Shalini555 <72308624+Shalini555@users.noreply.github.com>
2023-07-03 17:06:21 +0400

    DB-connection is done

---
+++ server.js
@@ -0,0 +1,48 @@
+var express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+const path = require("path");
+
+const errorHandler = require("./_helpers/error-handler");
+
+require("dotenv").config({ path: __dirname + "/.env" });
+
+/* import *
+ *  routes *
+ *    here */
+
+const MONGO_DB_PASSWORD = process.env["MONGO_DB_PASSWORD"];
+// const connectionString = `mongodb+srv://Admin:${MONGO_DB_PASSWORD}@icaf-cluster.pahle.mongodb.net/icafDB?retryWrites=true&w=majority`;
+const connectionString = `mongodb+srv://hospital_MS:${MONGO_DB_PASSWORD}@hospitalms.zw6pwlv.mongodb.net/?retryWrites=true&w=majority`;
+
+(app = express()), (port = process.env.PORT || 4000);
+app.use(bodyParser.json({ limit: "50mb" }));
+
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+/* global *
+ *  error *
+ *    handler */
+app.use(errorHandler);
+
+/* add *
+ *  routes *
+ *    here */
+//app.use('/', UserRoutes);
+
+mongoose
+  .connect(connectionString)
+  .then(() => {
+    app.listen(port, () => {
+      console.log(
+        "Server is listening on port " + port + `\n http://localhost:${port}`
+      );
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
